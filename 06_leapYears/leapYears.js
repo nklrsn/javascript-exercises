99e2e1355d53fbcfdb9890ea93c6775ea48fa4f6
nklrsn <177313118+nklrsn@users.noreply.github.com>
2024-10-12 17:50:30 -0400

    Complete sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    const isMultipleOf4 = year % 4 === 0;
+    const isMultipleOf100 = year % 100 === 0;
+    const isMultipleOf400 = year % 400 === 0;
 
+    return isMultipleOf4 && (!isMultipleOf100 || isMultipleOf400);
 };
 
 // Do not edit below this line
