932b72ba8c3d41430649dade74ae0ae8c0c220e0
nklrsn <177313118+nklrsn@users.noreply.github.com>
2024-10-12 17:02:20 -0400

    Complete third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    const array = string.split("");
+    const reversedArray = array.reverse();
+    const result = reversedArray.join("");
 
+    return result;
 };
 
 // Do not edit below this line
