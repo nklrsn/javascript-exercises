ad73464045c5a1f4b6e27c29456dc72fb80da714
nklrsn <177313118+nklrsn@users.noreply.github.com>
2024-10-12 17:39:09 -0400

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let result = 0;
+    let start;
+    let end;
+
+    if (!Number.isInteger(a) || a < 0 || !Number.isInteger(b) || b < 0) {
+        return "ERROR";
+    }
+
+    if (a <= b) {
+        start = a;
+        end = b;
+    } else {
+        start = b;
+        end = a;
+    }
+
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+
+    return result;
 
 };
 
